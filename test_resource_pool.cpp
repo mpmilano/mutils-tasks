95a50c164482609a1559d8890ea96ea33694dbfb
Matthew Milano <milano@cs.cornell.edu>
2016-08-01 18:29:49 -0400

    single-threaded is working; next test multi-threaded

--- test_resource_pool.cpp
+++ test_resource_pool.cpp
@@ -4,6 +4,7 @@
 using namespace mutils;
 
 int main(){
+	{
 	using LockedResource = typename ResourcePool<int>::LockedResource;
 	using WeakResource = typename ResourcePool<int>::WeakResource;
 	ResourcePool<int> pool{3,[](){return new int{0};}};
@@ -37,6 +38,7 @@ int main(){
 	assert(*prefer_1.lock() == 1);
 	assert(pool.dbg_pool_full());
 	WeakResource orphan{[&](const auto &, const auto &, const auto&){
+			assert(*pool.acquire() == 0);
 			return pool.acquire();
 		}(prefer_1.lock(), prefer_2.lock(),prefer_3.lock())};
 	auto val = *orphan.lock();
@@ -48,7 +50,8 @@ int main(){
 	if (*locked_orphan == 3){
 		assert(*prefer_2.lock() == 2);
 		assert(*prefer_1.lock() == 1);
-		assert(*prefer_3.lock() == 1 || *prefer_3.lock() == 2);
+		auto tmp = *prefer_3.lock();
+		assert(tmp == 1 || tmp == 2);
 	}
 	if (*locked_orphan == 2){
 		assert(*prefer_1.lock() == 1);
@@ -60,4 +63,6 @@ int main(){
 		assert(*prefer_2.lock() == 2);
 		assert(*prefer_3.lock() == 3);
 	}
+	assert(*pool.acquire() != 0);
+}
 }
