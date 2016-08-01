76e84198bf7d8592f943e047aad5467ac086a0c7
Matthew Milano <milano@cs.cornell.edu>
2016-08-01 02:02:04 -0400

    WIP resource pool

--- resource_pool.hpp
+++ resource_pool.hpp
@@ -8,17 +8,16 @@ namespace mutils{
 	
 	template<typename T, typename... Args>
 	class ResourcePool{
-		using resource_pair = std::pair<std::mutex, std::unique_ptr<T> >;
 		struct resource_pack{
 			std::mutex mut;
 			std::unique_ptr<T> resource;
 			const std::size_t index;
 			bool initialized{false};
-			resource_pack(std::size_t ind)
-				:resource{nullptr},index{ind}{}
+			resource_pack(std::size_t ind);
+			resource_pack(resource_pack&&);
 		};
 		
-		using resources_vector = std::vector<resource_pair>;
+		using resources_vector = std::vector<resource_pack>;
 		using size_type = typename resources_vector::size_type;
 		using lock = std::unique_lock<std::mutex>;
 
@@ -32,7 +31,7 @@ namespace mutils{
 			const size_type max_resources;
 			std::atomic<size_type> current_max_index{0};
 			SafeSet<size_type> recycled_indices;
-			SafeSet<resource_pair*> free_resources;
+			SafeSet<resource_pack*> free_resources;
 			const std::function<T* (Args...)> builder;
 			state(size_type max_resources, const decltype(builder) &builder);
 
@@ -45,17 +44,19 @@ namespace mutils{
 		};
 		
 		std::shared_ptr<state> _state;
+		
+	public:
 
 		ResourcePool(size_type max_resources, const decltype(state::builder) &builder);
 		
 		ResourcePool(const ResourcePool&) = delete;
-		
-	public:
 
 		struct index_owner{
 			const size_type indx;
 			std::shared_ptr<state> parent;
 			index_owner(const size_type indx, std::shared_ptr<state> parent);
+			index_owner(const index_owner&) = delete;
+			index_owner(index_owner&&);
 			~index_owner();
 		};
 
@@ -63,22 +64,23 @@ namespace mutils{
 			std::unique_ptr<T> t;
 			std::shared_ptr<state> parent;
 			const size_type index;
-			resource(std::unique_ptr<T> t, std::shared_ptr<state> parent, index_owner indx);
+			resource(std::unique_ptr<T> t, std::shared_ptr<state> parent, size_type indx);
 			~resource();
 		};
 		
 		class LockedResource{
-			const std::shared_ptr<const index_owner> index_preference;
+			std::shared_ptr<const index_owner> index_preference;
 			std::shared_ptr<state> parent;
-			std::shared_ptr<resource> resource;
+			std::shared_ptr<resource> rsource;
 
 			//for when this resource is unmanaged due to overfull pull
 			std::shared_ptr<std::unique_ptr<T> > single_resource;
 			
 		public:
 			LockedResource(const LockedResource&) = delete;
-			LockedResource(std::unique_ptr<T> t, std::shared_ptr<state> parent, index_owner indx);
-			LockedResource(std::unique_ptr<T> t);
+			LockedResource(std::unique_ptr<T> t, std::shared_ptr<state> parent, size_type indx);
+			LockedResource(std::unique_ptr<T> t, std::shared_ptr<state> parent);
+			LockedResource(LockedResource&& o);
 
 			T const * const operator->() const;
 			T* operator->();
@@ -87,19 +89,22 @@ namespace mutils{
 			const T& operator&() const;
 			
 			explicit LockedResource(const WeakResource& wr);
+			friend class WeakResource;
 
 		};
 		struct WeakResource{
-			const std::shared_ptr<const index_owner> index_preference;
+			std::shared_ptr<const index_owner> index_preference;
 			std::shared_ptr<state> parent;
-			std::weak_ptr<resource> resource;
+			std::weak_ptr<resource> rsource;
 			
 			//for when this resource is unmanaged due to overfull pull
 			std::weak_ptr<std::unique_ptr<T> > single_resource;
 		public:
 			LockedResource lock(Args && ... a);
 			WeakResource(const WeakResource&) = delete;
+			WeakResource(WeakResource&&);
 			explicit WeakResource(const LockedResource& lr);
+			friend class LockedResource;
 		};
 		
 		LockedResource acquire(Args && ... a);
--- resource_pool_impl.hpp
+++ resource_pool_impl.hpp
@@ -1,7 +1,19 @@
 #pragma once
+#include <cassert>
+#include "resource_pool.hpp"
 
 namespace mutils{
 
+	template<typename T, typename... Args>
+	ResourcePool<T,Args...>::resource_pack::resource_pack(std::size_t ind)
+				:resource{nullptr},index{ind}{}
+
+	template<typename T, typename... Args>
+	ResourcePool<T,Args...>::resource_pack::resource_pack(resource_pack&& o)
+		:resource(std::move(o.resource)),
+		 index(o.index),
+		 initialized(o.initialized){}
+	
 	template<typename T, typename... Args>
 	ResourcePool<T,Args...>::state::state(size_type max_resources, const decltype(builder) &builder)
 		:max_resources(max_resources),
@@ -13,6 +25,7 @@ namespace mutils{
 	
 	template<typename T, typename... Args>
 	bool ResourcePool<T,Args...>::state::pool_full() const {
+		assert(this);
 		return max_resources <= (current_max_index + 1)
 			|| recycled_indices.size() == 0;
 	}
@@ -20,18 +33,19 @@ namespace mutils{
 	template<typename T, typename... Args>	
 	typename ResourcePool<T,Args...>::LockedResource ResourcePool<T,Args...>::state::acquire_no_preference(std::shared_ptr<state> _this, Args && ... a){
 		//don't even try to get a preference
-		const static std::function<resource_pair* ()> nullfun = []()-> resource_pair* {return nullptr;};
-		auto *cand = _this->free_resources.build_or_pop(nullfun);
+		const static std::function<resource_pack* ()> nullfun = []()-> resource_pack* {return nullptr;};
+		auto *cand = _this->free_resources.
+			template build_or_pop<resource_pack*>(nullfun);
 		while (cand){
 			lock l{cand->mut};
 			if (cand->resource)
 				return
-					LockedResource{std::move(cand->resource),_this,index_owner{cand->index,_this}};
+					LockedResource{std::move(cand->resource),_this,cand->index};
 			else cand = _this->free_resources.build_or_pop(nullfun);
 		}
 		//whoops, entirely full!
 		//just build a one-off resource
-		return LockedResource{std::unique_ptr<T>{_this->builder(std::forward<Args>(a)...)}};
+		return LockedResource{std::unique_ptr<T>{_this->builder(std::forward<Args>(a)...)},_this};
 	}
 	
 	template<typename T, typename... Args>
@@ -40,7 +54,8 @@ namespace mutils{
 		if (!_this->pool_full()){
 			//try to get a preference!
 			auto my_preference = (_this->recycled_indices.size() > 0 ?
-								  _this->recycled_indices.build_or_pop(
+								  _this->recycled_indices.
+								  template build_or_pop<size_type>(
 									  [&cmi = _this->current_max_index](){
 										  return cmi.fetch_add(1);})
 								  : _this->current_max_index.fetch_add(1));
@@ -69,8 +84,7 @@ namespace mutils{
 		}
 		if (my_resource.resource){
 			return LockedResource{std::move(my_resource.resource),
-					_this,
-					index_owner{preference,_this}};
+					_this,preference};
 		}
 		else return acquire_no_preference(_this,std::forward<Args>(a)...);
 	}
@@ -84,77 +98,99 @@ namespace mutils{
 				:indx(indx),parent(parent){}
 
 	template<typename T, typename... Args>
+	ResourcePool<T,Args...>::index_owner::index_owner(index_owner&& o)
+		:indx(o.indx),parent(o.parent){
+		o.parent.reset();
+	}
+	
+	template<typename T, typename... Args>
 	ResourcePool<T,Args...>::index_owner::~index_owner(){
-		assert(indx < parent->max_resources);
-		parent->recycled_indices.add(indx);
+		if (parent){
+			assert(indx < parent->max_resources);
+			parent->recycled_indices.add(indx);
+		}
 	}
 
 	template<typename T, typename... Args>
-	ResourcePool<T,Args...>::resource::resource(std::unique_ptr<T> t, std::shared_ptr<state> parent, index_owner index)
-		:t(t),parent(parent){
+	ResourcePool<T,Args...>::resource::resource(std::unique_ptr<T> t, std::shared_ptr<state> parent, size_type index)
+		:t(std::move(t)),parent(parent),index(index){
 		assert(!parent->resources.at(index).resource);
 	}
 
 	template<typename T, typename... Args>
 	ResourcePool<T,Args...>::resource::~resource(){
-		assert(indx < max_resources);
+		assert(index < parent->max_resources);
 		lock l{parent->resources.at(index).mut};
 		parent->resources.at(index).resource = std::move(t);
 		parent->free_resources.add(&parent->resources.at(index));
 	}
 	
 	template<typename T, typename... Args>
-	ResourcePool<T,Args...>::LockedResource::LockedResource(std::unique_ptr<T> t, std::shared_ptr<state> parent, index_owner index)
-		:index_preference(new index_owner(indx,parent)),
+	ResourcePool<T,Args...>::LockedResource::LockedResource(std::unique_ptr<T> t, std::shared_ptr<state> parent, size_type indx)
+		:index_preference(new index_owner{indx,parent}),
 		 parent(parent),
-		 resource(std::move(t),parent,indx),
+		 rsource(new resource{std::move(t),parent,indx}),
 		 single_resource(nullptr)
 	{}
 
 	template<typename T, typename... Args>
-	ResourcePool<T,Args...>::LockedResource::LockedResource(std::unique_ptr<T> t)
+	ResourcePool<T,Args...>::LockedResource::LockedResource(std::unique_ptr<T> t, std::shared_ptr<state> parent)
 		:index_preference(nullptr),
-		 parent(nullptr),
-		 resource(nullptr),
+		 parent(parent),
+		 rsource(nullptr),
 		 single_resource(new std::unique_ptr<T>{std::move(t)})
 	{}
 
 	template<typename T, typename... Args>
+	ResourcePool<T,Args...>::LockedResource::LockedResource(LockedResource&& o)
+		:index_preference(o.index_preference),
+		 parent(o.parent),
+		 rsource(o.rsource),
+		 single_resource(o.single_resource)
+	{
+		o.index_preference.reset();
+		o.parent.reset();
+		o.rsource.reset();
+		o.single_resource.reset();
+	}
+
+	template<typename T, typename... Args>
 	T const * const ResourcePool<T,Args...>::LockedResource::operator->() const {
-		return (resource ? resource->t.get() : single_resource->t.get());
+		return (rsource ? rsource->t.get() : single_resource->get());
 	}
 	
 	template<typename T, typename... Args>
 	T* ResourcePool<T,Args...>::LockedResource::operator->() {
-		return (resource ? resource->t.get() : single_resource->t.get());
+		return (rsource ? rsource->t.get() : single_resource->get());
 	}
 	
 	template<typename T, typename... Args>
 	T& ResourcePool<T,Args...>::LockedResource::operator*() {
-		return (resource ? *resources->t : *single_resource->t);
+		return (rsource ? *rsource->t : *(*single_resource));
 	}
 	
 	template<typename T, typename... Args>
 	const T& ResourcePool<T,Args...>::LockedResource::operator&() const {
-		return (resource ? *resources->t : *single_resource->t);
+		return (rsource ? *rsource->t : *(*single_resource));
 	}
 			
 	
 	template<typename T, typename... Args>
-	explicit ResourcePool<T,Args...>::LockedResource::LockedResource(const WeakResource& wr)
+	ResourcePool<T,Args...>::LockedResource::LockedResource(const WeakResource& wr)
 		:index_preference(wr.index_preference),
 		 parent(wr.parent),
-		 resource(wr.resource.lock()),
+		 rsource(wr.rsource.lock()),
 		 single_resource(wr.single_resource.lock())
 	{
-		assert(resource || single_resource);
+		assert(rsource || single_resource);
 	}
 
 	template<typename T, typename... Args>
 	typename ResourcePool<T,Args...>::LockedResource ResourcePool<T,Args...>::WeakResource::lock(Args && ... a){
-		auto locked = resource.lock();
+		assert(parent);
+		auto locked = rsource.lock();
 		auto single_locked = single_resource.lock();
-		if ((locked && *locked) || (single_locked && *single_locked))
+		if ((locked && locked->t) || (single_locked && *single_locked))
 			return LockedResource(*this);
 		else if (index_preference){
 			return parent->acquire_with_preference(parent,index_preference->indx,std::forward<Args>(a)...);
@@ -165,15 +201,29 @@ namespace mutils{
 	}
 
 	template<typename T, typename... Args>
-	explicit ResourcePool<T,Args...>::WeakResource::WeakResource(const LockedResource& lr)
+	ResourcePool<T,Args...>::WeakResource::WeakResource(const LockedResource& lr)
 		:index_preference(lr.index_preference),
 		 parent(lr.parent),
-		 resource(lr.resource),
+		 rsource(lr.rsource),
 		 single_resource(lr.single_resource)
 	{}
 
 	template<typename T, typename... Args>
+	ResourcePool<T,Args...>::WeakResource::WeakResource(WeakResource&& o)
+		:index_preference(o.index_preference),
+		 parent(o.parent),
+		 rsource(o.rsource),
+		 single_resource(o.single_resource)
+	{
+		o.index_preference.reset();
+		o.parent.reset();
+		o.rsource.reset();
+		o.single_resource.reset();
+	}
+
+
+	template<typename T, typename... Args>
 	typename ResourcePool<T,Args...>::LockedResource ResourcePool<T,Args...>::acquire(Args && ... a){
-		return _state->acquire_with_preference(_state,max_resources + 1, std::forward<Args>(a)...);
+		return _state->acquire_new_preference(_state, std::forward<Args>(a)...);
 	}
 }
