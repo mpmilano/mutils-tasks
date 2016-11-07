3bdac5622f86281936a35743cd6ba77b75a70387
Matthew Milano <milano@cs.cornell.edu>
2016-11-07 03:56:41 -0500

    minor enhancements for eventfd

--- eventfd.cpp
+++ eventfd.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 namespace mutils {
 
-	eventfd::eventfd()
-		:fd(::eventfd(0,0)){}
+	eventfd::eventfd(bool block_on_first_wait)
+		:fd(::eventfd((block_on_first_wait ? 0 : 1),0)){}
 	eventfd::~eventfd(){
 		close(fd);
 	}
@@ -18,17 +18,21 @@ namespace mutils {
 		auto result =
 #endif
 			read(fd,&buf,sizeof(buf));
-		assert(result == 0);
+		assert(result == sizeof(buf));
 	}
 
-
+	void eventfd::clear(){
+		notify();
+		wait();
+	}
+	
 	void eventfd::notify(){
 		buf_t buf{1};
 #ifndef NDEBUG
 		auto result =
 #endif
 			write(fd,&buf,sizeof(buf));
-		assert(result == 0);
+		assert(result == sizeof(buf));
 	}
 
 	int eventfd::underlying_fd() const { return fd;}
--- eventfd.hpp
+++ eventfd.hpp
@@ -5,13 +5,15 @@ namespace mutils {
 	struct eventfd {
 		const int fd{};
 
-		eventfd();
+		eventfd(bool block_on_first_wait = true);
 		~eventfd();
 
 		using buf_t = unsigned long long;
 		static_assert(sizeof(buf_t) == 8,"error: eventfd returns exactly 8 bytes");
 
 		void wait();
+
+		void clear();
 
 		void notify();
 		
