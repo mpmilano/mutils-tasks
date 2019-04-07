9b5ca3841627af4bcb075ba27c4e42bad4d753ba
Weijia Song <songweijia@gmail.com>
2019-04-07 04:39:06 +0000

    fix include and lib paths

--- mutils-tasksConfig.cmake
+++ mutils-tasksConfig.cmake
@@ -1,1 +1,2 @@
-set(mutils_tasks_LIBRARIES "-lmutils-tasks")
+set(mutils-tasks_INCLUDE_DIRS "${CMAKE_INSTALL_PREFIX}/include")
+set(mutils-tasks_LIBRARIES "-L${CMAKE_INSTALL_PREFIX}/lib -lmutils-tasks")
